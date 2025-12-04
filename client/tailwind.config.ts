11a2e64e13860226a1dcfdbc85cabc38600c2b55
newghzzz666 <github@zzxj.netlib.re>
2025-12-04 23:55:33 +0800

    Update tailwind.config.ts

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,5 +1,3 @@
-import type { Config } from 'tailwindcss'
-
 export default {
   content: [
     "./index.html",
@@ -50,4 +48,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config
+}
